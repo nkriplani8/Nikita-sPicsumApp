0970ef7577f301eacb943fe88d798428734c5228
finnynj <finny.nj@gmail.com>
2021-02-22 12:44:35 +0530

    upload

--- app/src/main/java/com/example/imageshow/MainActivity.java
+++ app/src/main/java/com/example/imageshow/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initializing...
         gridView = findViewById(R.id.gridView);
 
         Retrofit retrofit=new Retrofit.Builder()
